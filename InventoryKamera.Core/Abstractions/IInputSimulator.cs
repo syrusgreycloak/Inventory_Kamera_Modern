3b8508e30c9ad14af0bebb1ffdd8045159cb6c1d
Karl Nickels <karl.nickels@citizen2a.com>
2026-04-13 17:57:04 -0400

    refactor: expand IInputSimulator with MouseVerticalScroll, SystemWait, SetDelay, navigation actions

--- InventoryKamera.Core/Abstractions/IInputSimulator.cs
+++ InventoryKamera.Core/Abstractions/IInputSimulator.cs
@@ -2,11 +2,24 @@ namespace InventoryKamera
 {
     public interface IInputSimulator
     {
+        // Existing
         void Click();
         void Click(int x, int y);
         void SetCursor(int x, int y);
         void ScrollDown(int scrolls);
         void ScrollUp(int scrolls);
         void Wait(int ms);
+
+        // New
+        void MouseVerticalScroll(int clicks);
+        void SystemWait(ScanDelay delay = ScanDelay.Normal);
+        void SetDelay(double ms);
+        double GetDelay();
+
+        // Game navigation actions
+        void SelectNextCharacter();
+        void SelectCharacterAttributes();
+        void SelectCharacterConstellation();
+        void SelectCharacterTalents();
     }
 }
--- InventoryKamera/Infrastructure/WindowsInputSimulator.cs
+++ InventoryKamera/Infrastructure/WindowsInputSimulator.cs
@@ -2,34 +2,19 @@ namespace InventoryKamera.Infrastructure
 {
     internal class WindowsInputSimulator : IInputSimulator
     {
-        public void Click()
-        {
-            Navigation.Click();
-        }
-
-        public void Click(int x, int y)
-        {
-            Navigation.Click(x, y);
-        }
-
-        public void SetCursor(int x, int y)
-        {
-            Navigation.SetCursor(x, y);
-        }
-
-        public void ScrollDown(int scrolls)
-        {
-            Navigation.Scroll(Navigation.Direction.DOWN, scrolls);
-        }
-
-        public void ScrollUp(int scrolls)
-        {
-            Navigation.Scroll(Navigation.Direction.UP, scrolls);
-        }
-
-        public void Wait(int ms)
-        {
-            Navigation.Wait(ms);
-        }
+        public void Click() => Navigation.Click();
+        public void Click(int x, int y) => Navigation.Click(x, y);
+        public void SetCursor(int x, int y) => Navigation.SetCursor(x, y);
+        public void ScrollDown(int scrolls) => Navigation.Scroll(Navigation.Direction.DOWN, scrolls);
+        public void ScrollUp(int scrolls) => Navigation.Scroll(Navigation.Direction.UP, scrolls);
+        public void Wait(int ms) => Navigation.Wait(ms);
+        public void MouseVerticalScroll(int clicks) => Navigation.sim.Mouse.VerticalScroll(clicks);
+        public void SystemWait(ScanDelay delay = ScanDelay.Normal) => Navigation.SystemWait((Navigation.Speed)(int)delay);
+        public void SetDelay(double ms) => Navigation.SetDelay(ms);
+        public double GetDelay() => Navigation.GetDelay();
+        public void SelectNextCharacter() => Navigation.SelectNextCharacter();
+        public void SelectCharacterAttributes() => Navigation.SelectCharacterAttributes();
+        public void SelectCharacterConstellation() => Navigation.SelectCharacterConstellation();
+        public void SelectCharacterTalents() => Navigation.SelectCharacterTalents();
     }
 }
